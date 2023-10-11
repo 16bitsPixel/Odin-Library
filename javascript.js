1925868169995272484738e1f6e2ffb6b7b97a8a
16bitsPixel <brandonllanes16@gmail.com>
2023-10-10 21:59:48 -0700

    add basic site structure

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+// to store books
+const myLibrary = [];
+
 // constructor for Book objects
 function Book(title, author, numPages, readYet) {
     // book variables
@@ -19,5 +22,46 @@ function Book(title, author, numPages, readYet) {
     }
 }
 
-let book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(book1.info());
+// adds a book to library
+function addBookToLibrary(title, author, numPages, readYet) {
+    let book = new Book(title, author, numPages, readYet);
+    myLibrary.push(book);
+}
+
+// displays books onto page
+function displayLibrary() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        // create the card background
+        let bookCard = document.createElement("div");
+        bookCard.classList.add("card");
+
+        // add text onto the card
+        let titleText = document.createElement("p");
+        titleText.textContent = myLibrary[i].title;
+        let authorText = document.createElement("p");
+        authorText.textContent = myLibrary[i].author;
+        let pageText = document.createElement("p");
+        pageText.textContent = myLibrary[i].numPages;
+        let readText = document.createElement("p");
+        if (myLibrary[i].readYet) {
+            readText.textContent = "Read";
+        }
+        else {
+            readText.textContent = "Not Read";
+        }
+
+        bookCard.append(titleText);
+        bookCard.append(authorText);
+        bookCard.append(pageText);
+        bookCard.append(readText);
+
+        document.querySelector(".library").append(bookCard);
+    }
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit2", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit3", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit4", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit5", "J.R.R. Tolkien", 295, false);
+displayLibrary();
